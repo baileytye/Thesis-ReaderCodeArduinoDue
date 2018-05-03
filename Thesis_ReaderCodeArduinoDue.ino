a7a1e18dc6bda472efca9dcad50917c6851986b3
baileytye <bazle10552@yahoo.com>
2018-05-03 14:26:53 -0600

    Added current reader code

--- Thesis_ReaderCodeArduinoDue.ino
+++ Thesis_ReaderCodeArduinoDue.ino
@@ -1,12 +1,252 @@
+/* File: Reader.ino
+ * Created by: Bailey Tye
+ * Date Created: 3/22/2018
+ * Last Edited: 4/2/2018
+ */
+
+
+
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
+
+
+
+//-----------------------DEFINES------------------------//
+#define SIGNAL_PIN A0
+
+//ADC value of offset you consider to be a rising edge
+#define RISING_OFFSET 35
+
+//Variance of readings[] you consider to be on
+#define VARIANCE_THRESHOLD 100
+
+//Interval between bits of data
+#define BIT_INTERVAL 1277
+
+//Start interval offset
+#define START_INTERVAL_OFFSET 1362
+
+//Number of bits to receive
+#define BYTES_TO_RECEIVE 12
+
+//Set to 1 to read 60 ADC values and print them to the monitor
+#define READ_60 0
+
+
+unsigned int readings[15];
+
+byte data[BYTES_TO_RECEIVE];
+
+
+
+//-------------------INITIALIZATION--------------------//
+
 void setup()
 {
 // Add your initialization code here
+	Serial.begin(115200);
+
+//Make sure the ADC is running in its fastest mode
+	int reg = REG_ADC_MR;
+	Serial.print("REG_ADC_MR = ");
+	Serial.println(reg, HEX);
+
+	REG_ADC_MR = (REG_ADC_MR & 0xFFF0FFFF) | 0x00020000;
+	Serial.print("Changed to REG_ADC_MR = ");
+	Serial.println(reg, HEX);
+
+//Set resolution to 12 bits
+	analogReadResolution(12);
+
 }
 
-// The loop function is called in an endless loop
+//-----------------END OF INITIALIZATION-----------------//
+
+
+//---------------------MAIN LOOP-------------------------//
+
+
 void loop()
 {
-//Add your repeated code here
+
+	uint32_t startTime;
+	uint32_t endTime;
+
+
+
+
+
+	if(READ_60){
+		unsigned int r[60];
+		for(int i = 0; i < 60; i++){
+			r[i] = analogRead(SIGNAL_PIN);
+		}
+
+		//Display values
+		for(int i = 0; i < 60; i ++){
+			Serial.println(r[i]);
+		}
+
+		//Only execute once
+		while(1);
+	} else {
+
+		while(1){
+			//Wait till a rising edge is detected
+			Serial.println("//---------Waiting for Data--------// ");
+			waitForRising();
+			startTime = micros();
+
+			//Serial.println(readings[1] - readings[0]);
+
+			//Make sure its real
+			readings[0] = analogRead(SIGNAL_PIN);
+			readings[1] = analogRead(SIGNAL_PIN);
+			readings[2] = analogRead(SIGNAL_PIN);
+			readings[3] = analogRead(SIGNAL_PIN);
+			readings[4] = analogRead(SIGNAL_PIN);
+			readings[5] = analogRead(SIGNAL_PIN);
+			readings[6] = analogRead(SIGNAL_PIN);
+			readings[7] = analogRead(SIGNAL_PIN);
+			readings[8] = analogRead(SIGNAL_PIN);
+			readings[9] = analogRead(SIGNAL_PIN);
+			readings[10] = analogRead(SIGNAL_PIN);
+			readings[11] = analogRead(SIGNAL_PIN);
+
+
+
+			if(getVariance(readings, 12) < VARIANCE_THRESHOLD){
+				continue;
+			}
+
+			//Serial.println(getVariance(readings, 6));
+
+			//Wait till data starts
+			while((micros() - startTime) < (START_INTERVAL_OFFSET));
+
+			//Read data
+			readData();
+
+
+			for(int i = 0; i < BYTES_TO_RECEIVE;i++){
+				Serial.print(data[i],HEX);
+				Serial.print(" ");
+			}
+
+			Serial.print("\n");
+
+
+			for(int i = 0; i < BYTES_TO_RECEIVE; i++){
+				Serial.print(char(data[i]));
+			}
+
+			Serial.print("\n");
+			delay(500);
+		}
+
+	}
+
+	delay(1);
+
+
 }
+//--------------------END OF MAIN LOOP--------------------//
+
+
+
+
+//-----------------------FUNCTIONS------------------------//
+
+//Waits till a rising edge is detected
+void waitForRising(){
+
+	do {
+		readings[0] = analogRead(SIGNAL_PIN);
+		readings[1] = analogRead(SIGNAL_PIN);
+		readings[1] = analogRead(SIGNAL_PIN);
+		readings[1] = analogRead(SIGNAL_PIN);
+
+	} while(readings[1] < (readings[0] + RISING_OFFSET));
+
+}
+
+//Reads a byte bits of data
+void readData(){
+
+	uint32_t startTime;
+
+	for(int j = 0; j < BYTES_TO_RECEIVE; j++){
+	for(int i = 0; i < 8; i++){
+
+		startTime = micros();
+
+		readings[0] = analogRead(SIGNAL_PIN);
+		readings[1] = analogRead(SIGNAL_PIN);
+		readings[2] = analogRead(SIGNAL_PIN);
+		readings[3] = analogRead(SIGNAL_PIN);
+		readings[4] = analogRead(SIGNAL_PIN);
+		readings[5] = analogRead(SIGNAL_PIN);
+		readings[6] = analogRead(SIGNAL_PIN);
+		readings[7] = analogRead(SIGNAL_PIN);
+		readings[8] = analogRead(SIGNAL_PIN);
+		readings[9] = analogRead(SIGNAL_PIN);
+		readings[10] = analogRead(SIGNAL_PIN);
+		readings[11] = analogRead(SIGNAL_PIN);
+		readings[12] = analogRead(SIGNAL_PIN);
+		readings[13] = analogRead(SIGNAL_PIN);
+		readings[14] = analogRead(SIGNAL_PIN);
+
+
+		Serial.println(getVariance(readings, 15));
+
+		if(getVariance(readings, 15) > VARIANCE_THRESHOLD)
+			data[j] |= 1 << (7 - i);
+
+		//Serial.println(micros()-startTime);
+		while((micros() - startTime) < BIT_INTERVAL);
+
+	}
+	}
+
+
+}
+
+//Gets the average of array
+double getAverage(unsigned int array[], int length){
+
+	double average = 0;
+
+	for(int i = 0; i < length; i++){
+		average += array[i];
+	}
+
+	average /= length;
+
+	return average;
+}
+
+//Gets the variance of array, given the average
+double getVariance(double average, unsigned int array[], int length){
+
+	double variance = 0;
+	for(int i = 0; i < length; i++){
+		variance += abs((double)array[i] - average);
+	}
+	return variance;
+}
+
+//Calculates average then gets the variance of array
+double getVariance(unsigned int array[], int length){
+
+	double average = getAverage(array, length);
+
+	double variance = 0;
+	for(int i = 0; i < length; i++){
+		variance += abs((double)array[i] - average);
+	}
+	return variance;
+}
+
+
+
+
+
--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,13 +2,18 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-05-03 14:14:35
+//This file has been generated on 2018-05-03 14:26:26
 
 #include "Arduino.h"
 #include "Arduino.h"
 
 void setup() ;
 void loop() ;
+void waitForRising();
+void readData();
+double getAverage(unsigned int array[], int length);
+double getVariance(double average, unsigned int array[], int length);
+double getVariance(unsigned int array[], int length);
 
 #include "Thesis_ReaderCodeArduinoDue.ino"
 
